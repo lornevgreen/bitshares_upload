c9e9351650644420301cdaf7ba0db96511a18dad
Dipen Chauhan <get.dipen@gmail.com>
2018-06-13 09:39:33 -0700

    Root is welcome index

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   get 'welcome/index'
   get 'welcome/review'
   get 'welcome/upload'
+  root :to => "welcome#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
