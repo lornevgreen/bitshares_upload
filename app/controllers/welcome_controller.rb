eb24053d9e12ff0831c93a15004a5064c22558b8
Dipen Chauhan <get.dipen@gmail.com>
2018-06-13 09:15:01 -0700

    Created welcome controller and views

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,10 @@
+class WelcomeController < ApplicationController
+  def index
+  end
+
+  def review
+  end
+
+  def upload
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+  get 'welcome/review'
+  get 'welcome/upload'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
