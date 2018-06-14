8275305f21421ea3fe46ada92324c3b5ecd566fd
Dipen Chauhan <get.dipen@gmail.com>
2018-06-13 20:49:57 -0700

    Save uploaded file

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -6,5 +6,10 @@ class WelcomeController < ApplicationController
   end
 
   def upload
+	uploaded_io = params["cloud_coin_file"]
+	File.open(Rails.root.join('public', 'uploads', uploaded_io.original_filename), 'wb') do |file|
+		file.write(uploaded_io.read)
+	end
+	redirect_to welcome_review_url
   end
 end
