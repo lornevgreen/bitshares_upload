68f6e26fab254d50e0e6fded0225f35f6acac2cc
Dipen Chauhan <get.dipen@gmail.com>
2018-06-16 18:47:14 -0700

    Upload the file

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,13 +3,20 @@ class WelcomeController < ApplicationController
   end
 
   def review
+    puts "****"
+    sleep 5
+    puts params.inspect
   end
 
   def upload
-	uploaded_io = params["cloud_coin_file"]
-	File.open(Rails.root.join('public', 'uploads', uploaded_io.original_filename), 'wb') do |file|
-		file.write(uploaded_io.read)
-	end
-	redirect_to welcome_review_url
+    uploaded_io = params["cloud_coin_file"]
+    generated_file_name = Time.now.strftime("%Y%m%d%H%M%S") + uploaded_io.original_filename
+    File.open(Rails.root.join('public', 'uploads', generated_file_name), 'wb') do |file|
+      file.write(uploaded_io.read)
+    end
+    # File has been uploaded
+    
+    
+    redirect_to controller: "welcome", action: "review", f: generated_file_name
   end
 end
