12103526656bb112bb78789f43f0bc618ca84fc5
Dipen Chauhan <get.dipen@gmail.com>
2018-07-17 12:11:34 -0700

    Corrected filename

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -5,9 +5,14 @@ class NotificationMailer < ApplicationMailer
   #
   #   en.notification_mailer.download_file.subject
   #
-  def download_file
-    @greeting = "Hi"
-
-    mail to: "to@example.org"
+  
+  # Sends stack file to user
+  # @param  email             stack file will be sent to this email   
+  # @param  withdraw_amount   amount of cloud coins that are being sent
+  # @param  file_path         filepath of the stack file  
+  def download_email(email, file_path, withdraw_amount)
+    @withdraw_amount = withdraw_amount
+    attachments["cc" + withdraw_amount.to_s + ".stack"] = File.read(file_path)
+    mail(to: email, subject: "Your Cloudcoins Can Be Downloaded")
   end
 end
