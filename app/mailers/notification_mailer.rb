c448f1700cd26f6bc51748e8d61be0d0bba99e75
Dipen Chauhan <get.dipen@gmail.com>
2018-08-21 10:51:34 -0700

    Added deposit_email

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -15,4 +15,10 @@ class NotificationMailer < ApplicationMailer
     attachments["cc" + withdraw_amount.to_s + ".stack"] = File.read(file_path)
     mail(to: email, subject: "Your Cloudcoins Can Be Downloaded")
   end
+
+  def deposit_email(email, bitshares_account, deposit_amount)
+    @deposit_amount = deposit_amount
+    @bitshares_account = bitshares_account
+    mail(to: email, subject: "Your Cloudcoins Will be Transferred to Bitshares")
+  end
 end
