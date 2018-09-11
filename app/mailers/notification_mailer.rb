b8e12a5b53bee7acc69425b7597bb04838ae863b
Dipen Chauhan <get.dipen@gmail.com>
2018-09-11 16:41:44 -0700

    added username

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -6,11 +6,14 @@ class NotificationMailer < ApplicationMailer
   #   en.notification_mailer.download_file.subject
   #
   
+  default from: 'info@cloudcoin.global'
+  
   # Sends stack file to user
   # @param  email             stack file will be sent to this email   
   # @param  withdraw_amount   amount of cloud coins that are being sent
   # @param  file_path         filepath of the stack file  
-  def download_email(email, file_path, withdraw_amount)
+  def download_email(username, email, file_path, withdraw_amount)
+    @username = username
     @withdraw_amount = withdraw_amount
     @email = email
     attachments["cc" + withdraw_amount.to_s + ".stack"] = File.read(file_path)
