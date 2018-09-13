c9143f469ff6410f965436fbe57358c01c8c4a90
Dipen Chauhan <get.dipen@gmail.com>
2018-09-13 13:14:30 -0700

    Added modal, inputfile, spinner and tooltips

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,3 +17,74 @@
 //= require popper
 //= require bootstrap
 //= require_tree .
+
+// From https://codepen.io/overdrivemachines/pen/PdEZxZ
+// JQuery 3.3.1
+// Popper.js 1.14.3. Will not work with 1.14.4
+// Bootstrap 4.1.3
+// TODO: Replace Popper with Tippyjs
+// blue = #338fff
+// blue border = #007bff
+// https://www.flaticon.com/search?style_id=14
+
+$(function() {
+  // Stop playing video when Modal is closed
+  //https://youtu.be/dQw4w9WgXcQ
+  $youtubeModal = $("#videoModal");
+  $youtubeModal.on("hidden.bs.modal", function() {
+    var $this = $(this).find("iframe"),
+      tempSrc = $this.attr("src");
+    $this.attr("src", "");
+    $this.attr("src", tempSrc);
+  });
+
+  //   Input File...
+  $(".inputfile").each(function() {
+    var $input = $(this),
+      $label = $input.next("label"),
+      labelVal = $label.html();
+
+    $input.on("change", function(e) {
+      var fileName = "";
+
+      if (this.files && this.files.length > 1)
+        fileName = (this.getAttribute("data-multiple-caption") || "").replace(
+          "{count}",
+          this.files.length
+        );
+      else if (e.target.value) fileName = e.target.value.split("\\").pop();
+
+      if (fileName) $label.find("span").html(fileName);
+      else $label.html(labelVal);
+    });
+
+    // Firefox bug fix
+    $input
+      .on("focus", function() {
+        $input.addClass("has-focus");
+      })
+      .on("blur", function() {
+        $input.removeClass("has-focus");
+      });
+  });
+  
+  //   Show spinner...
+  $(".show-spinner").click(function() {
+    $(this).prop("disabled", true);
+    $("form input").prop("disabled", true);
+    $("label.btn").addClass("disabled");
+    var $icon = $("i", this );
+    $icon.removeClass("fa-upload");
+    $icon.addClass("fa-cog fa-spin" );
+    // $(".form-inner").addClass("spinner");
+  });
+  
+  // Enable tooltips everywhere
+  $('[data-toggle="tooltip"]').tooltip();
+  
+  $(window).on('load', function() {
+		var preLoder = $(".loader-wrapper");
+		preLoder.delay(700).fadeOut(500);
+		$('body').addClass('loaded');
+	});
+});
