bce5951bf73f86ce0177f263f624a2aa8863c0f3
Dipen Chauhan <get.dipen@gmail.com>
2018-09-14 12:22:24 -0700

    Flash to bootstrap class

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  # https://gist.github.com/suryart/7418454#gistcomment-970347
+  def bootstrap_class_for(flash_type)
+    {
+      success: 'alert-success',
+      error: 'alert-danger',
+      alert: 'alert-warning',
+      notice: 'alert-primary'
+    }[flash_type.to_sym] || flash_type.to_s
+  end
 end
