f883da831d17d1fa72970119ff7b99331882e4b3
Dipen Chauhan <get.dipen@gmail.com>
2018-09-17 21:44:24 -0700

    Added helper that calculates cloud coin value

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,16 @@ module ApplicationHelper
       notice: 'alert-primary'
     }[flash_type.to_sym] || flash_type.to_s
   end
+
+  def cloudcoin_value(serial_no)
+  	value = 0
+  	case serial_no
+  	when 1..2097152 then value = 1
+  	when 2097153..4194304 then value = 5
+  	when 4194305..6291456 then value = 25
+  	when 6291457..14680064 then value = 100
+  	when 14680065..16777217 then value = 250
+  	end
+  	return value
+  end
 end
