099d0173ab90a05835e1765766ea65fed97d7007
Dipen Chauhan <get.dipen@gmail.com>
2018-10-11 12:14:06 -0700

    Added withdraw index

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,16 @@
 # == Route Map
 #
-#                     Prefix Verb URI Pattern                                                                              Controller#Action
-#                       root GET  /                                                                                        deposit#index
-#              deposit_index GET  /deposit/index(.:format)                                                                 deposit#index
-#          deposit_completed GET  /deposit/completed(.:format)                                                             deposit#completed
-#             deposit_upload POST /deposit/upload(.:format)                                                                deposit#upload
-#             withdraw_index GET  /withdraw/index(.:format)                                                                withdraw#index
-#         withdraw_completed GET  /withdraw/completed(.:format)                                                            withdraw#completed {:format=>"json"}
-#            welcome_deposit GET  /welcome/deposit(.:format)                                                               welcome#deposit
-#  welcome_deposit_completed GET  /welcome/deposit_completed(.:format)                                                     welcome#deposit_completed
-#             welcome_upload POST /welcome/upload(.:format)                                                                welcome#upload
-#           welcome_withdraw GET  /welcome/withdraw(.:format)                                                              welcome#withdraw
-# welcome_withdraw_completed GET  /welcome/withdraw_completed(.:format)                                                    welcome#withdraw_completed {:format=>"json"}
-#            welcome_summary GET  /welcome/summary(.:format)                                                               welcome#summary {:format=>"json"}
-#         rails_service_blob GET  /rails/active_storage/blobs/:signed_id/*filename(.:format)                               active_storage/blobs#show
-#  rails_blob_representation GET  /rails/active_storage/representations/:signed_blob_id/:variation_key/*filename(.:format) active_storage/representations#show
-#         rails_disk_service GET  /rails/active_storage/disk/:encoded_key/*filename(.:format)                              active_storage/disk#show
-#  update_rails_disk_service PUT  /rails/active_storage/disk/:encoded_token(.:format)                                      active_storage/disk#update
-#       rails_direct_uploads POST /rails/active_storage/direct_uploads(.:format)                                           active_storage/direct_uploads#create
+#                    Prefix Verb URI Pattern                                                                              Controller#Action
+#                      root GET  /                                                                                        deposit#index
+#             deposit_index GET  /deposit/index(.:format)                                                                 deposit#index
+#         deposit_completed GET  /deposit/completed(.:format)                                                             deposit#completed
+#            deposit_upload POST /deposit/upload(.:format)                                                                deposit#upload
+#            withdraw_index GET  /withdraw/index(.:format)                                                                withdraw#index
+#        rails_service_blob GET  /rails/active_storage/blobs/:signed_id/*filename(.:format)                               active_storage/blobs#show
+# rails_blob_representation GET  /rails/active_storage/representations/:signed_blob_id/:variation_key/*filename(.:format) active_storage/representations#show
+#        rails_disk_service GET  /rails/active_storage/disk/:encoded_key/*filename(.:format)                              active_storage/disk#show
+# update_rails_disk_service PUT  /rails/active_storage/disk/:encoded_token(.:format)                                      active_storage/disk#update
+#      rails_direct_uploads POST /rails/active_storage/direct_uploads(.:format)                                           active_storage/direct_uploads#create
 
 Rails.application.routes.draw do
 	# You should put the root route at the top of the file, because it is 
@@ -29,7 +22,7 @@ Rails.application.routes.draw do
     get 'deposit/completed'
     post 'deposit/upload'
 
-    # get 'withdraw/index'
+    get 'withdraw/index'
     # get 'withdraw/completed', defaults: {format: 'json'}
 
     # Old routes:
